85f3ede550b5273b6418f7af23d0c49e769cf2b2
nico17022001 <nico17022001boolean@gmail.com>
2023-04-27 17:55:31 +0200

    aggiunto-svg

--- src/components/main.vue
+++ src/components/main.vue
@@ -10,9 +10,41 @@ export default {
 </script>
 
 <template>
+  <!-- CONTENITORE MAIN -->
   <div id="main-wrapper">
+    <!-- CONTENITORE PRINCIPALE -->
     <div id="contenuto-principale" class="black-background"></div>
-    <div id="barra-info" class="blu-teambg"></div>
+    <!-- BARRA INFO -->
+    <div id="barra-info" class="blu-teambg">
+      <!-- CONTENITORE DELLE SVG -->
+      <div id="contenitore-svg">
+        <!-- BOX INFO -->
+        <div class="svg-box">
+          <div class="svg"><img class="normal" src="../assets/img/buy-comics-digital-comics.png" alt=""></div>
+          <div class="svgName">DIGITAL COMICS</div>
+        </div>
+
+        <div class="svg-box">
+          <div class="svg"><img class="normal" src="../assets/img/buy-comics-merchandise.png" alt=""></div>
+          <div class="svgName">DC MERCHANDISE</div>
+        </div>
+
+        <div class="svg-box">
+          <div class="svg"><img class="normal" src="../assets/img/buy-comics-subscriptions.png" alt=""></div>
+          <div class="svgName">SUBSCRIPTION</div>
+        </div>
+
+        <div class="svg-box">
+          <div class="svg"><img class="w30" src="../assets/img/buy-comics-shop-locator.png" alt=""></div>
+          <div class="svgName">COMIC SHOP LOCATOR</div>
+        </div>
+
+        <div class="svg-box">
+          <div class="svg"><img class="normal" src="../assets/img/buy-dc-power-visa.svg" alt=""></div>
+          <div class="svgName">DC POWER VISA</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -31,5 +63,27 @@ export default {
   #barra-info{
     height: 110px;
     width: 100%;
+    display: flex;
+    justify-content: center;
+  }#contenitore-svg{
+    width: 75%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }.svg-box{
+    display: flex;
+    align-items: center;
+  }.svgName{
+    font-size: small;
+    margin-left: 10px;
+    color: white;
+  }
+
+  .svg{
+
+  }img{
+
+  }.normal{
+    width: 40px;
   }
 </style>
